4af30a7b273da5bf22f032e280d7ffc17efb39e9
Jianqing_Sun <sunjianqing@gmail.com>
2018-03-30 16:44:26 -0700

    more code

--- src/main/scala/com/jianqing/scala/netflix/ParquetDemo.scala
+++ src/main/scala/com/jianqing/scala/netflix/ParquetDemo.scala
@@ -1,6 +1,8 @@
 package com.jianqing.scala.netflix
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 /**
   * Created by jianqingsun on 3/29/18.
@@ -23,6 +25,7 @@ object ParquetDemo extends App {
       ("USA", "ipad", 2),
       ("CAN", "mac", 3)))
 
+
     val rdd1 = sc.parallelize(Seq(
       ("CHN", "iphone", 1),
       ("CHN", "iphone", 2),
@@ -30,8 +33,20 @@ object ParquetDemo extends App {
       ("USA", "ipad", 2),
       ("CAN", "mac", 4)))
 
+    val schema: StructType = StructType(
+      Array(
+        StructField(name = "country", dataType = StringType, nullable = false),
+        StructField(name = "type", dataType = StringType, nullable = false),
+        StructField(name = "id", dataType = IntegerType, nullable = false))
+    )
 
-    rdd.intersection(rdd1).map(x => println(x._1 + " " + x._2 + " " + x._3)).collect()
+    // Create DataFrame from RDD
+    val rowRdd: RDD[Row] = rdd.map(r => Row(r._1, r._2, r._3))
+    val dataFrame = mySpark.createDataFrame(rowRdd, schema)
 
+
+    // Write out as Parquet file
+    val filePath = "src/test/resource/parquet/data.parquet"
+    dataFrame.write.option("compression","none").mode("overwrite").parquet(filePath)
   }
 }
